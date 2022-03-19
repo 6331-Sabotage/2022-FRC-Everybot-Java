9d5eaaf09d1fccf7acf26b7b8b676e806e9d0a81
JWL4402 <j.lebert4402@gmail.com>
2022-03-19 15:47:16 -0400

    made the components constant variables

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,9 +29,9 @@ public class Robot extends TimedRobot {
 	// XboxController xbox = new XboxController(0);
 	Joystick input = new Joystick(0);
 
-	Drivetrain drivetrain = new Drivetrain(0.5);
-	Intake intake = new Intake(0.5);
-	Limelight limelight = new Limelight();
+	final Drivetrain drivetrain = new Drivetrain(0.5);
+	final Intake intake = new Intake(0.5);
+	final Limelight limelight = new Limelight();
 	
 	/**
 	* This function is run when the robot is first started up and should be used for any
@@ -92,7 +92,7 @@ public class Robot extends TimedRobot {
 	/** This function is called periodically during operator control. */
 	@Override
 	public void teleopPeriodic() {
-		drivetrain.drive(input.getRawAxis(1), input.getRawAxis(0));
+		// drivetrain.drive(input.getRawAxis(1), input.getRawAxis(0));
 		intake.run(input.getRawButton(1), input.getRawButton(2));
 		Values limelightValues = limelight.getValues();
 		limelight.post();
